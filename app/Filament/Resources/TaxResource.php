2a366957426d2573e86e225050e2132b9e670c6c
joao.castro8 <joao.castro8@estudante.ifto.edu.br>
2025-05-14 13:22:38 +0000

    commit inicial

---
+++ app/Filament/Resources/TaxResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TaxResource\Pages;
+use App\Filament\Resources\TaxResource\RelationManagers;
+use App\Models\Tax;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TaxResource extends Resource
+{
+    protected static ?string $model = Tax::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTaxes::route('/'),
+            'create' => Pages\CreateTax::route('/create'),
+            'edit' => Pages\EditTax::route('/{record}/edit'),
+        ];
+    }
+}
